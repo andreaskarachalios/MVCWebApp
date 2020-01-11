050e75b4542a0b20813b90f2c10c86030dd207b4
andreaskarachalios <karachalios.andreas@gmail.com>
2020-01-11 18:35:10 +0200

    First commit

---
+++ Models/Source.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace MyWebApp.Models {
+    public class Film {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public Genre[] Genres { get; set; }
+        public bool IsInStore { get; set; }
+    }
+
+
+    public enum Genre {
+        Action,
+        Adventure,
+        Comedy,
+        Drama,
+        War
+    }
+
+}
